142ec04121db4f889efb5f5b300dd4dbfd38451a
Helmut Wolf <helmut.wolf@outlook.com>
2015-01-05 12:52:46 +0100

    Added k-Folding cross-validation

--- amazonSimilarities.m
+++ amazonSimilarities.m
@@ -1,29 +1,63 @@
-%% load files
+%% configuration
+clc;
 clear all;
+nFolds = 5;
+neighbourhoodSize = 25;
+treshold = 2.2;
+buyValue = 0.75;
+
+%% load files
 display('loading data');
-buyValue = 2;
-rankings = loadData(buyValue, 'data/test.mat');
+
+rankings = loadData(buyValue, '../data/tiny.mat');
 
 %% initialization
 productCount = size(rankings, 2);
-trainingData = 0.5;
-neighbourhoodSize = 25;
-trainingIndices = 1:(trainingData*productCount);
-testIndices = (trainingData*productCount + 1):productCount;
-treshold = 1.1;
 
-%% calculation
-tic;
-display('build model');
-similarityCache = buildModel(rankings, trainingIndices, neighbourhoodSize, 0);
+column_vector = zeros(1, size(rankings,2));
+indices = crossvalind('Kfold', column_vector, nFolds);
+
+precision = zeros(nFolds);
+recall = zeros(nFolds);
+evaluation_score = zeros(nFolds);
 
 display('estimate rankings');
-[rankingsEstimated, rankingsCorrect] = estimateRanking(rankings, similarityCache, testIndices, treshold, buyValue);
-toc;
+for i = 1:nFolds
+    testIndices = (indices == i);
+    trainingIndices = (indices ~= i); 
+    [~, ~, testColumnsIndices] = find(testIndices' .* (1:size(testIndices)));
+    [~, ~, trainColumnsIndices] = find(trainingIndices' .* (1:size(trainingIndices)));
+    
+    tic;
+    display('build model');
+    similarityCache = buildModel(rankings, trainColumnsIndices, neighbourhoodSize, 0);
+    toc;
+    
+    display(' ')
+    display(strcat('Fold #', num2str(i)));
+    
+    tic
+    [rankingsEstimated, rankingsCorrect] = estimateRanking(rankings, similarityCache, sort(testColumnsIndices), treshold, buyValue);
+    toc;
+    
+    %% checks
+    display('check accuracy');
+    [precision(i), recall(i)] = calculateMetrics(rankingsCorrect, rankingsEstimated);
 
-%% checks
-display('check accuracy');
-[precision, recall] = calculateMetrics(rankingsCorrect, rankingsEstimated);
+    display(strcat('Precision:',num2str(precision(i))));
+    display(strcat('Recall:',num2str(recall(i))));
+end
 
-display(strcat('precision:',num2str(precision)));
-display(strcat('recall:',num2str(recall)));
+precision_avg= mean(precision);
+recall_avg = mean(recall);
+evaluation_score_avg = mean(evaluation_score);
+
+display('=======================================================');
+display(strcat('Averaged results after ', num2str(nFolds), ' runs:'));
+display(strcat('Precision: ', num2str(precision_avg(1))));
+display(strcat('Recall: ', num2str(recall_avg(1))));
+display(strcat('Evaluation Score: ', num2str(evaluation_score_avg(1))));
+
+
+
+
