f5324078ddaec5bf2612c1f5067b4cce9cbba0a2
Helmut Wolf <helmut.wolf@outlook.com>
2015-01-05 13:22:19 +0100

    removed buildmodel.m

--- buildModel.m
+++
@@ -1,5 +0,0 @@
-function [similarityCacheReduced] = buildModel(rankings, trainingIndices, neighbourhoodSize, subtractAverageRankings)
-    similarityCache = calculateSimilarities(rankings, subtractAverageRankings);
-    similarityCacheReduced = reduceSimilarityCache(similarityCache, trainingIndices, neighbourhoodSize);
-end
-
--- precisionCalculation.m
+++ precisionCalculation.m
@@ -1,13 +1,17 @@
 function [precision] = precisionCalculation(buyValue, treshold)
-    rankings = loadData(buyValue, 'data/test.mat');
+    rankings = loadData(buyValue, '../data/test.mat');
     productCount = size(rankings, 2);
     trainingData = 0.5;
     neighbourhoodSize = 25;
     trainingIndices = 1:(trainingData*productCount);
     testIndices = (trainingData*productCount + 1):productCount;
     
-    similarityCache = buildModel(rankings, trainingIndices, neighbourhoodSize, 0);
-    [rankingsEstimated, rankingsCorrect] = estimateRanking(rankings, similarityCache, testIndices, treshold, buyValue);
+    similarityCache = calculateSimilarities(rankings, 0);
+    similarityCacheReduced = reduceSimilarityCache(similarityCache, trainingIndices, neighbourhoodSize);
+    
+    
+    
+    [rankingsEstimated, rankingsCorrect] = estimateRanking(rankings, similarityCacheReduced, testIndices, treshold, buyValue);
     
     [precision, recall] = calculateMetrics(rankingsCorrect, rankingsEstimated);
     display(strcat('treshold:',num2str(treshold),', buyValue:',num2str(buyValue),', precision:',num2str(precision),', recall:',num2str(recall)));
