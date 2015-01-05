18e27da7ccb959b2e913fa10b530743d48095888
Benedikt Schmidt <benediktibk@aon.at>
2015-01-05 11:45:05 +0100

    improved division by the column sums

--- estimateRanking.m
+++ estimateRanking.m
@@ -5,10 +5,16 @@ function [rankingsEstimated, rankingsCorrect] = estimateRanking(rankings, simila
     rankingsEstimated = sparse([], [], [], testIndicesCount, userCount);
     testProductSimilarities = similarities(:, testIndices);
     testProductSimilaritySums = sum(testProductSimilarities);
+    [nonZeroSumRows, nonZeroSumColumns, nonZeroSums] = find(testProductSimilaritySums > 0);
+    nonZeroCount = size(nonZeroSums, 1);
+    
+    for i = 1:nonZeroCount
+        testProductSimilarities(:, nonZeroSumColumns(i)) = testProductSimilarities(:, nonZeroSumColumns(i))./nonZeroSums(i);
+    end
     
     for user = 1:userCount
         currentRankings = rankings(user, :);
-        estimation = (currentRankings*testProductSimilarities)./testProductSimilaritySums;
+        estimation = currentRankings*testProductSimilarities;
         [rows, columns] = find(estimation > treshold);
         rankingsEstimated(columns, user) = buyValue;
     end
