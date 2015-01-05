72a05c8559b64172a1fcf4335c87b08d5a6b0f8b
Benedikt Schmidt <benediktibk@aon.at>
2015-01-05 11:31:33 +0100

    removed unused function

--- reshapeUpperTriangular.m
+++
@@ -1,7 +0,0 @@
-function [rows, columns, values] = reshapeUpperTriangular(A, minimalValue)
-    n = size(A, 1);  
-    reduction = A > minimalValue;
-    AReduced = A.*reduction;
-    [rows, columns, values] = find(AReduced);
-end
-
